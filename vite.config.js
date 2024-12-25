684d61ca9b34a6cb68c4305dbfe036a95b157ba6
Monika Kubianka <mkubianka@gmail.com>
2024-12-25 12:24:06 +0100

    Fix GitHub Pages paths

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,21 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Portfolio/", // Ustaw podkatalog na GitHub Pages
   plugins: [react(), svgr],
   server: {
     port: 3000,
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import svgr from "vite-plugin-svgr";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), svgr],
+//   server: {
+//     port: 3000,
+//   },
+// });
